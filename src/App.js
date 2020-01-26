063a415c6072ba6415cb6e18f655baed826f6ac8
manjil thapa <manjilthapa@manjils-MacBook-Pro.local>
2020-01-26 23:05:33 +0100

    used link and dynamic routing to demonstrate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Link} from 'react-router-dom'
+
+const HomePage = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <Link to='/articles'>Go To Articles</Link>
+      <button onClick={()=>props.history.push('/articles')}>Articles</button>
+      <h1>HOME PAGE</h1>
+    </div>
+  )
+}
+const ArticlePage = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <h1>ARTICLES LIST PAGE</h1>
+      <p>Dynamic routing using match properties</p>
+      <p><Link to={`${props.match.url}/1`}>To article 1</Link></p>
+      <p><Link to={`${props.match.url}/2`}>To article 2</Link></p>
+      <p><Link to={`${props.match.url}/3`}>To article 3</Link></p>
+    </div>
+  )
+  }
+
+const ArticleDetailPage = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <h2> ARTICLE DETAIL PAGE: {props.match.params.articleId}</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/articles" component={ArticlePage}/>
+      <Route exact path="/articles/:articleId" component={ArticleDetailPage}/>
     </div>
   );
 }
